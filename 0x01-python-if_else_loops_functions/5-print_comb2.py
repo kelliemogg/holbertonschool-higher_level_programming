d4e42335631ca0e81ca6dbf3505fa8f5521b1d49
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-08 21:00:48 +0000

    only two prints allowed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for c in range(0, 100):
-    if c < 10:
-        print("0{}, ".format(c), end="")
-    elif c > 9 and c < 99:
-        print("{}, ".format(c), end="")
+    if c < 99:
+        print("{:02d}, ".format(c), end="")
     elif c == 99:
         print("{}".format(c))
