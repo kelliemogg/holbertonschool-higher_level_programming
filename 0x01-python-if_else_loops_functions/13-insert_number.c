683f5f476ca17d60a38381a2d8179f6468d83e6e
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-09 04:07:48 +0000

    push

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert
+ * description: insert
+ * @head: head
+ * @number: number
+ * Return: 0
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	return(0);
+}
