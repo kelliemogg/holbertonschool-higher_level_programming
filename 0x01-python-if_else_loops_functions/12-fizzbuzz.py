9157834b35c6e1a5b48179c072f2fa7489736644
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-09 03:55:29 +0000

    commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+for num in range (1, 100):
+    if num % 3 and num % 5
+        print("FizzBuzz")
+    if num % 3
+        print("Fizz")
+    if num % 5
+        print("Buzz")
