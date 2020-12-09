1ab583b55a24f4110b403171009cc12cc3db994d
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-09 20:19:52 +0000

    added chr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@ def uppercase(str):
     for c in str:
         a = ord(c)
         if c > 96 and c < 123:
-            c = chr(c)
+            y -= 32
+        c = chr(c)
         print("{}".format(c), end="")
     print("")
