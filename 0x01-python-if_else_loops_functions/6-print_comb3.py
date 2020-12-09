6e65a0a8688377437e1bced3bc0b88d051aefa5f
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-09 20:07:35 +0000

    correct output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-for f in range(0, 9):
-    for l in range(0, 9):
-        if f is not l:
-            print("{}{}, ".format(f, l), end="")
+for f in range(0, 10):
+    for l in range(0, 10):
+        if f is not l and f is not 8:
+            if l - f <= 0:
+                continue
+            else:
+                print("{}{}, ".format(f, l), end="")
         elif (f is 8) and (l is 9):
             print("{}{}".format(f, l))
