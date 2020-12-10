43e4a21da682630ea0c09f36ae2dc2bc0826af30
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-10 19:42:54 +0000

    multiple functions imported at once

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
 
-    from calculator_1 import add
     add = add(a, b)
     print("{} + {} = {}".format(a, b, add))
 
-    from calculator_1 import sub
     sub = sub(a, b)
     print("{} - {} = {}".format(a, b, sub))
 
-    from calculator_1 import mul
     mul = mul(a, b)
     print("{} * {} = {}".format(a, b, mul))
 
-    from calculator_1 import div
     div = div(a, b)
     print("{} / {} = {}".format(a, b, div))
