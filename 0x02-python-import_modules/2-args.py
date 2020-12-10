0ef63932adcf3cbf492bf06e25720ef0238326ca
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-10 23:12:43 +0000

    enumerate to second condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     if len(argv) is 1:
         print("0 arguments.")
     elif len(argv) is 2:
-        print("1 argument:\n1: {}".format(argv[1:]))
+        for position, arg in enumerate(argv[1:]):
+            print("1 argument:\n1: {}".format(arg))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for position, arg in enumerate(argv[1:]):
