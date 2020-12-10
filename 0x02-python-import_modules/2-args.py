5e86000e9b31f8ae1ba478b532bcae2e4c9f91f2
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-10 23:08:26 +0000

    for loop in final condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    for i in range(0, len(argv)):
-        if len(argv) is 1:
-            print("0 arguments.")
-        elif len(argv) is 2:
-            print("1 argument:\n1: {}".format(argv[1:]))
-            break:
-        else:
-            print("{} arguments:".format(len(argv) - 1), end="")
-            print("\n{}: {}".format(i + 1, argv[1:]))
+    if len(argv) is 1:
+        print("0 arguments.")
+    elif len(argv) is 2:
+        print("1 argument:\n1: {}".format(argv[1:]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for position, arg in enumerate(argv[1:]):
+            print("{}: {}".format(position + 1, arg))
