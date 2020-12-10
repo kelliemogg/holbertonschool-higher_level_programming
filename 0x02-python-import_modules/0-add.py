cc928df7d8e7b538ea0ce800b6fd6893797e7c3d
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-10 19:05:37 +0000

    import name as main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
-    def add(a, b):
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, value))
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = add(a, b)
+    print("{} + {} = {}".format(a, b, sum))
