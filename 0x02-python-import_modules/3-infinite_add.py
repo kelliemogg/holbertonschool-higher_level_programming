0327b4dd5ec87922e9cf1a81f4f143898e9bb203
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-11 00:51:42 +0000

    correct output using int()

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+my_sum = 0
+num = len(argv)
+
+if num > 2:
+    for i in range(1, num):
+        argv[i] = int(argv[i])
+        my_sum += argv[i]
+    print(my_sum)
