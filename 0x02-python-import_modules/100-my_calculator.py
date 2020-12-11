a7cb423978f21a6fe2fa66dc317524db02305fca
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-11 04:39:31 +0000

    omg it works

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,17 +3,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
     args = len(argv)
-    a = int(argv[1])
-    b = int(argv[3])
-    add = add(a, b)
-    sub = sub(a, b)
-    mul = mul(a, b)
-    div = div(a, b)
 
     if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        a = int(argv[1])
+        b = int(argv[3])
+        add = add(a, b)
+        sub = sub(a, b)
+        mul = mul(a, b)
+        div = div(a, b)
         if argv[2] == '+':
             print("{} + {} = {}".format(a, b, add))
         elif argv[2] == '-':
