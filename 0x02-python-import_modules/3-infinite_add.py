36f627a12fc12717ddf2a4e290b727a540fa3e5d
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-11 00:53:28 +0000

    if name then main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-my_sum = 0
-num = len(argv)
+if __name__ == "__main__":
+    from sys import argv
+    my_sum = 0
+    num = len(argv)
 
-if num > 2:
-    for i in range(1, num):
-        argv[i] = int(argv[i])
-        my_sum += argv[i]
-    print(my_sum)
+    if num > 2:
+        for i in range(1, num):
+            argv[i] = int(argv[i])
+            my_sum += argv[i]
+        print(my_sum)
