c96d6be35eb8983450d05ed3349e73d6b6123902
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-11 04:25:14 +0000

    correct input

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 from sys import argv
-from add_0 import add
 operator = {"+", "-", "*", "/"}
 args = len(argv)
 a = int(argv[1])
 b = int(argv[3])
-result = add(a, b)
+add = add(a, b)
+sub = sub(a, b)
+mul = mul(a, b)
+div = div(a, b)
 
 if args != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 elif args == 4:
-    for i in operator:
-        if argv[2] != {"+", "-", "*", "/"}:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            print("{} {} {} = {}".format(a, operator, b, result))
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add))
+    elif argv[2] == '-':
+            print("{} + {} = {}".format(a, b, sub))
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul))
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
