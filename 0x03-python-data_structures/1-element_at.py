f4b32acc9ffcfdfc2e51dfd9bfc301cfc7912318
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-16 03:50:06 +0000

    -1 to account for 0th pos

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for x in range(len(my_list)):
-        if idx < 0:
-            return(None)
-        elif idx > (len(my_list)):
-            return(None)
-        else:
-            return(idx + 1)
+    if idx < 0:
+        return(None)
+    elif idx > (len(my_list) - 1):
+        return(None)
+    else:
+        return(idx + 1)
