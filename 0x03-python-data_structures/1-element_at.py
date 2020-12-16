d3c8b84a4306bcdf1ecf3cc945998d8a5d983428
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-16 03:33:55 +0000

    correct output

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for x in range(len(my_list)):
         if idx < 0:
             return(None)
-        elif idx > my_list[x]:
+        elif idx > (len(my_list)):
             return(None)
         else:
-            return(idx)
+            return(idx + 1)
