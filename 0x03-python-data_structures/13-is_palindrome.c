0e557ee6668589f9596a5e45e72fcd9fe82e10c3
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-16 05:29:51 +0000

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - pali?
+ * description: is it a palindrome
+ * @head: head of list
+ * Return: 0 or 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *tail;
+
+	if (*head == NULL)
+		return (1);
+	else
+	{
+		tail = *head;
+	while (tail->next != NULL)
+		tail = tail->next;
+	if (tail != head)
+		return (0);
+	}
+}
