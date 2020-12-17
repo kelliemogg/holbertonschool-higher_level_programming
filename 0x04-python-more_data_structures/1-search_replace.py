10f62e6d1941145d3bc5c8f1e87a61c17e81c919
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-17 22:03:51 +0000

    null check first

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if not my_list:
+            return
     new_list = my_list.copy()
     for i in my_list:
-        if not my_list:
-            return(my_list)
-        elif my_list[i] == search:
+        if my_list[i] == search:
             new_list[i] = replace
             return(new_list)
