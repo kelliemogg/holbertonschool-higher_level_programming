539d4a9a779aee3b65c2d289f7c9c90d44859938
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-17 22:07:17 +0000

    added range and took return out of if

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     if not my_list:
             return
     new_list = my_list.copy()
-    for i in my_list:
+    for i in range(0, len(my_list)):
         if my_list[i] == search:
             new_list[i] = replace
-            return(new_list)
+    return(new_list)
