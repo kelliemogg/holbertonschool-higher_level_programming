4b1ecd8d6196826de1d47c3bee26d9bf76c95be7
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-18 16:53:30 +0000

    remove for loop

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    total = 0
     new_dict = a_dictionary.copy()
 #    for key in new_dict:
 #        count = new_dict[key]
 #        total += count
-        for key in new_dict:
-            new_dict[key] = (new_dict[key] * 2)
-        return(new_dict)
+    for key in new_dict:
+        new_dict[key] = (new_dict[key] * 2)
+    return(new_dict)
