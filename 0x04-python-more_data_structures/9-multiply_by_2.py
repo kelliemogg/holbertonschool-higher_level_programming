46c9a3cea5957701c0a3020026e1dbbb1d8a11a7
Kellie Mogg <kelliedmogg@gmail.com>
2020-12-18 15:28:42 +0000

    made a copy of dict

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for x in a_dictionary:
-        a_dictionary[x] = a_dictionary[x] * 2
-        return(a_dictionary)
+    total = 0
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
+        count = new_dict[key]
+        total += count
+        for key in new_dict:
+            new_dict[key] = (new_dict[key] * 2)
+        return(new_dict)
