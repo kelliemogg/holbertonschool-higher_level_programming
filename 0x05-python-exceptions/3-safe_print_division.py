066d61c9b826739cf34c09d0cf5b43002a05469e
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-05 00:33:22 +0000

    dividing but still an error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
     try:
-        res = a / b
+        result = a / b
     except ZeroDivisionError:
         print("Inside result: None")
+    else:
+        print("Inside result: {}".format(result))
     finally:
-        print("Inside result: {}".format(res))
+        return (result)
