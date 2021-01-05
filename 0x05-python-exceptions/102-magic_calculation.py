a2ce8648fae3d19542dbe1a3cfdbde3805118152
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-05 21:08:46 +0000

    just curious

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
-for i in range (1, 103)
-if a > i
-except:
-print("Too far")
-result (a, b)
+    return 0
+
+# for i in range (1, 103)
+# if a > i
+# except:
+# print("Too far")
+# result (a, b)
