5e936d5908a3f82f5d2c410322b472b175c1577f
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-05 23:59:42 +0000

    if raise rather than try except

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,10 @@
 class Square:
     """ Sq Class defines sq by size, a private instance attribute """
     def __init__(self, size=0):
-        try:
-            print("{:d}".format(size))
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
