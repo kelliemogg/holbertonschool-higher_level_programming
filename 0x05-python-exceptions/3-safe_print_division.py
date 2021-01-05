a23faa78da99090c7e1508854ca73a7207b8c9ce
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-05 01:11:26 +0000

    saved result as none for error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-    else:
-        print("Inside result: {}".format(result))
+        result = None
     finally:
+        print("Inside result: {}".format(result))
         return (result)
