8aca30ef7ae472d7b36c3f561a555595cdabc880
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-06 00:21:41 +0000

    not quite

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module Goes Here """
+
+
+class Square:
+    """ Sq Class defines sq by size, a private instance attribute """
+
+    def __init__(self, size=0):
+        size = self.__size
+
+    @property
+    def size(self):
+        return (size)
+
+    @setter
+    def size(self, size):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        area = (self.__size) * (self.__size)
+        return(area)
