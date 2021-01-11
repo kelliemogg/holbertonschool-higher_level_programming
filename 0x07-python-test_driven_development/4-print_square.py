717090ba5f35a45f7bd7577e4b7f2b2ce409c51b
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-11 01:20:19 +0000

    setting up file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+""" function that adds 2 integers """
+
+
+def print_square(size):
+    """ function that prints a square """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return(int(a) + int(b))
