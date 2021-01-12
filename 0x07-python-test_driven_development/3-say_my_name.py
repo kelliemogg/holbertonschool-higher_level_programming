d0b62a54f3a7b71041f1c2bfbc98e6912d85c06f
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-12 04:10:58 +0000

    correct output

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,9 +3,14 @@
 
 
 def say_my_name(first_name, last_name=""):
+
     """ this function prints a name """
+
     if type(first_name) != str:
         TypeError("first_name must be a string")
-    if type(last_name) != str:
+    elif last_name and type(last_name) != str:
         TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    elif last_name is None:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
