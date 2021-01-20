a48cc7499450fee4c3daa233c1de8937e7ce1b82
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 05:29:35 +0000

    description

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,8 +3,12 @@
 
 
 class MyInt(int):
+    """ reverse equals and not equals """
+
     def __eq__(self, other):
+        """ reverse equals """
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """ reverse not equals """
         return super().__eq__(other)
