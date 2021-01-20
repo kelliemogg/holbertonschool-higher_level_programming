ea27e80058de1b980d131110704193bb2a8bd7b1
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 05:44:58 +0000

    pep8

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ add a new attribute to an obj if possible """
 
+
 def add_attribute(obj, name, value):
     """ check if obj can have new attribute """
     if '__dict__' not in dir(obj):
