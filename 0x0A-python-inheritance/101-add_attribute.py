e945bce2847888e7178043aa4a6f9e118fde403f
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 05:44:15 +0000

    using __dict__ func

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ add a new attribute to an obj if possible """
 
-def add_attribute():
+def add_attribute(obj, name, value):
     """ check if obj can have new attribute """
-    raise TypeError("can't add new attribute")
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
