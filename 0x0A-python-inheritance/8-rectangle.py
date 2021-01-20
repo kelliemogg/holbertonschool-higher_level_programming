490d4ce7f514fb2c280ac7720b1924bd4c272d98
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 04:43:31 +0000

    assign import to basegeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,7 +2,7 @@
 """ This is no longer an empty class """
 
 
-__import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Applies geometry to Rectangles """
