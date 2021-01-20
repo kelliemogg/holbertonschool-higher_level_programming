c9271dce92baebc41f24137d739e092c995c78a0
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 01:44:09 +0000

    self instead of list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     """ List contains elements of type int """
 
-    def print_sorted(MyList):
+    def print_sorted(self):
         """ Prints the list in ascending order """
-        print(sorted(MyList))
+        print(sorted(self))
