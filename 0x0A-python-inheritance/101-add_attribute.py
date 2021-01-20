8e419fb4b42c9c12ab80b67f85739dd9c6e929c1
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 05:36:04 +0000

    base file

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" add a new attribute to an obj if possible """
+
+def add_attribute():
+    """ check if obj can have new attribute """
+    raise TypeError("can't add new attribute")
