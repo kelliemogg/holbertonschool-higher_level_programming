00980b0bdb9dfa46aed6c569ec8e2d544bd3fd24
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-20 05:03:20 +0000

    SUPER

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,55 +2,16 @@
 """ This is no longer an empty class """
 
 
-class BaseGeometry:
-    """ This function is for geometry """
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+Rectangle = __import__('9-rectangle').Rectangle
 
-    def area(self):
-        """ Defines the area """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Checks for type errors """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.value = value
-
-
-class Rectangle(BaseGeometry):
-    """ Applies geometry to Rectangles """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-    def integer_validator(self, name, value):
-        """ Checks for type errors """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.value = value
 
 class Square(Rectangle):
-    """ Applies geo to Square """
+    """ Applies geometry to squares """
     def __init__(self, size):
-        self.size = size
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-    def integer_validator(self, name, value):
-        """ Checks for type errors """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.value = value
-
-        def area(self):
-            """ Defines the area """
-            raise Exception("area() is not implemented")
+    def area(self):
+        """ define area of obj """
+        return self.__size * self.__size
