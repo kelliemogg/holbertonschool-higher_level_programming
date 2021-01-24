04a05a0727fd60992b47283786aea3807f288a23
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-24 22:00:33 +0000

    correct output on 11 / need to test for width == height

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,21 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """ getter for size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter for size """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 1:
+            raise ValueError('width must be > 0')
+        else:
+            self.__size = value
+
     def __str__(self):
         """ output of square attributes """
         return "[Square] ({}) {}/{} - {}".format(
