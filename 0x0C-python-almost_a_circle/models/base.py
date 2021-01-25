693cdf6a74b2d7c3d7cc070e7e93555c710d44d2
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-25 16:26:43 +0000

    returns JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ base module to manage id attributes in future classes"""
 
 
+import json
+
+
 class Base:
     """ This class will be the base for all other classes in this project """
     __nb_objects = 0
@@ -14,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            json_string = json.dumps(list_dictionaries, default = str)
+            return json_string
