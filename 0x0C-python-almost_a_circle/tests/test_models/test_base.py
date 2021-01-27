93b30fbf26fe6019e668d9ffb482db39de5e033f
Kellie Mogg <kelliedmogg@gmail.com>
2021-01-27 03:22:26 +0000

    test_models

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" module containts unit tests for class Base """
+
+
+import unittest
+import json
+
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ unit tests for Base """
+
+    def test_id(self):
+        """ is id and id update functional """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base(5)
+        self.assertEqual(b2.id, 5)
+
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
+
+        b4 = Base(-5)
+        self.assertEqual(b4.id, -5)
+
+        b5 = Base()
+        self.assertEqual(b5.id, 2)
+
+        b6 = Base(None)
+        self.assertEqual(b6.id, 3)
+
+        b7 = Base(3.14)
+        self.assertEqual(b7.id, 3.14)
+
+    def test_for_none_to_json_string(self):
+        """ test for none """
+        self.assertEqual(Base.to_json_string(None), '[]')
+
+    def test_for_none_from_json_string(self):
+        """ test for none """
+        self.assertEqual(Base.from_json_string(None), '[]')
+
+    def test_to_json_string(self):
+        """ test JSON string conversion """
+        b1_dict = {'id': 4, 'width': 5, 'height': 10, 'x': 3, 'y': 3}
+        j_string = Base.to_json_string(b1_dict)
+        t_d = json.loads(j_string)
+        self.assertEqual(t_d, b1_dict)
+        self.assertIs(type(j_string), str)
+        self.assertIs(type(t_d), dict)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module containts unit tests for class Square """
+
+
+import unittest
+import json
+
+from models.square import Square
+
+
+class SquareBase(unittest.TestCase):
+    """ unit tests for class Square """
+
+    @classmethod
+    def setUpClass(self):
+        """ set up Square """
+        pass
