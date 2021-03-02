2090d49d0c145a654f567aa99b531fca34f166b2
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-02 20:00:22 +0000

    Create a user and set password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates the MySQL server user user_0d_1
+-- Create password for the new user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
