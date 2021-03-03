d5fdf889da1b3a01c8a8eb6d91542a8b2fed7425
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-03 04:18:16 +0000

    .

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Create hbtn_0d_tvshows
+-- USE it
+-- Import the database from hbtn_0d_tvshows
+CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
+USE hbtn_0d_tvshows;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows;
