147a988c12dae9e2c84fd4232a837305249b2a07
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-03 22:59:05 +0000

    Left Join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- List all shows and all genres linked to that show from hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows JOIN tv_show_genres
+FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name ASC;
