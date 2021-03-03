eec28167e8c825b901c3842c1f171bbd039a2187
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-03 22:34:31 +0000

    .

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN cities ON states ORDER BY cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
