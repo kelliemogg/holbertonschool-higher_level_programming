9c2bd45dc1234e6c81d4e45a4219b290104dbe5a
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-03 18:48:12 +0000

    Join without join

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- Lists all the cities of California that can be found in hbtn_0d_usa
-SELECT id FROM cities WHERE state_id = 'California' ORDER BY cities.id ASC;
+SELECT id, name
+FROM cities WHERE state_id = 1 ORDER BY cities.id ASC;
