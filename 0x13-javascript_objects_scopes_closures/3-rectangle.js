d4566ea753896be66f91255b955e12d73d880564
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-23 20:04:13 +0000

    Printing more squares but using repeat

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const x = 'X';
+    for (let i = 1; i <= this.height; i++) {
+      console.log(x.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
