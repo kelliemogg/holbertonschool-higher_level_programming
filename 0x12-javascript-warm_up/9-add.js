7cbfe90f0d4a4a67d040fa519b41a37a0628b047
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-23 15:00:41 +0000

    Adding using parseInt

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const one = process.argv[2];
 const two = process.argv[3];
-const res = add(one, two);
 
-function add (a, b) {
-  return a + b;
+function add (one, two) {
+  return parseInt(one) + parseInt(two);
 }
 
 if (process.argv.length < 4) {
   console.log('NaN');
 } else {
+  const res = add(one, two);
   console.log(res);
 }
