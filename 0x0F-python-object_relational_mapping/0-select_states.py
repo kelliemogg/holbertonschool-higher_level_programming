c45d205575df4629fcc68a95647f3f5ebcc19809
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-25 21:12:29 +0000

    Don't use git add 0-select_states.py! in your passwords

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module imports MySQLdb and returns a sorted list of states by id"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3])
+
+c = serv.cursor()
+
+c.execute("SELECT * FROM states ORDER BY states.id ASC")
+fields = c.fetchall()
+
+for field in fields:
+    print(field)
+
+serv.close()
+c.close()
