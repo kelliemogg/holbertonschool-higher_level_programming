fb6ec5159fd8cbaa701b4e2749e622c99844b94d
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-25 22:13:12 +0000

    Filter by N states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module imports filterd objects from a database """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+c = serv.cursor()
+
+c.execute("SELECT * FROM states ORDER BY states.id ASC")
+fields = c.fetchall()
+
+for field in fields:
+    if field[1].startswith("N"):
+        print(field)
+
+c.close()
+serv.close()
