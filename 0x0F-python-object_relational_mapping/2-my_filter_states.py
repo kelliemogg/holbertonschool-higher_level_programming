3905503270fdedcafedb3e7189adcad85fc11de4
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-25 23:05:43 +0000

    Filter by a certain state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module imports filterd objects from a database """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+c = serv.cursor()
+arg = argv[4]
+
+c.execute("SELECT * FROM states WHERE states.name LIKE '{}' ORDER BY states.id".format(arg))
+fields = c.fetchall()
+
+for field in fields:
+        print(field)
+
+c.close()
+serv.close()
