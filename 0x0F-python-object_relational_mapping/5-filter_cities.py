b1e475d6c856f612b10dfcc4f3d6308c3f1a04bb
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-26 17:44:53 +0000

    List of cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Sorts cities by state in a database """
+
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+serv = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3])
+
+c = serv.cursor()
+arg = argv[4]
+clean = []
+
+c.execute(
+    "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY states.id ASC;", (arg,))
+fields = c.fetchall()
+
+for field in fields:
+    clean += field
+print(', '.join(clean))
+
+c.close()
+serv.close()
