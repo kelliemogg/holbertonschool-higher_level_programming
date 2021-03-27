545e4bd47b5bde0b01f4b5070c9a53f96519fafa
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-27 00:53:23 +0000

    delete states that contain a certain character

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lists the first state objects from the database hbtn_0e_6_usa """
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    import MySQLdb
+    from sys import argv
+    from model_state import Base, State
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+
+for state in session.query(State).filter(State.name.like('%a%')).all():
+    session.delete(state)
+
+session.commit()
+session.close()
