414695675787594d6f85fbbc507617263847389d
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-27 00:27:39 +0000

    Adding and commiting new data

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Lists the first state objects from the database hbtn_0e_6_usa """
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    import MySQLdb
+    from sys import argv
+    from model_state import Base, State
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
+.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+Louise = State(id='6', name='Louisiana')
+session.add(Louise)
+session.commit()
+
+print("{}".format(Louise.id))
+
+session.close()
