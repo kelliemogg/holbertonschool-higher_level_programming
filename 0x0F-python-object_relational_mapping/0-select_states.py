3ebdd3cc3c7e022eafbe7beb1b61e80b2b760a00
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-29 02:46:09 +0000

    spacing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """ This module imports MySQLdb and returns a sorted list of states by id """
 
+if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
-if __name__ == '__main__':
-
-serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3])
 
-c = serv.cursor()
+    c = serv.cursor()
 
-c.execute("SELECT * FROM states ORDER BY states.id ASC")
-fields = c.fetchall()
-for field in fields:
-    print(field)
+    c.execute("SELECT * FROM states ORDER BY states.id ASC")
+    fields = c.fetchall()
+    for field in fields:
+        print(field)
 
-# close all cursors
-serv.close()
-# close db
-c.close()
+    serv.close()
+    c.close()
