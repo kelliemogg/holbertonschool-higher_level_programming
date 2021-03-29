fa08a80d8dcc199af5d7d8721ff1831bab6ba056
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-29 02:50:31 +0000

    documentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,25 +6,23 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
-serv = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3])
+    serv = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
 
-""" this is to connect """
+    c = serv.cursor()
+    arg = argv[4]
 
-c = serv.cursor()
-arg = argv[4]
+    c.execute(
+        "SELECT * FROM states WHERE states.name LIKE '{}' ORDER BY states.id"
+        .format(arg))
+    fields = c.fetchall()
 
-c.execute(
-    "SELECT * FROM states WHERE states.name LIKE '{}' ORDER BY states.id"
-    .format(arg))
-fields = c.fetchall()
+    for field in fields:
+        print(field)
 
-for field in fields:
-    print(field)
-
-c.close()
-serv.close()
+    c.close()
+    serv.close()
