77731fcc45074ea113555faf83261767497bd4fe
Kellie Mogg <kelliedmogg@gmail.com>
2021-03-29 02:38:06 +0000

    doc string

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module imports MySQLdb and returns a sorted list of states by id"""
+""" This module imports MySQLdb and returns a sorted list of states by id """
 
     import MySQLdb
     from sys import argv
@@ -10,10 +10,12 @@ serv = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                        passwd=argv[2], db=argv[3])
 
 c = serv.cursor()
+
 c.execute("SELECT * FROM states ORDER BY states.id ASC")
 fields = c.fetchall()
 for field in fields:
     print(field)
+
 # close all cursors
 serv.close()
 # close db
