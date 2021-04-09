5b508eba09752f9f3d78adc926bc85b5a75cd046
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-09 23:47:21 +0000

    requests.post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fetches a website using requests packages """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    r = requests.post(url, data)
+    print(r.text)
+    
