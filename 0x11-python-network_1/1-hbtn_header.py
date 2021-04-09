21c09a1f3e1087e7c37816af9c39c40859d93a48
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-09 19:18:25 +0000

    add if name is main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,9 @@
 """ script to take in a url, send a request to the URL, and
 displays the value of the X-Request-Id variable """
 
-
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
 url = sys.argv[1]
 
