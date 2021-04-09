6065d2449b75a3993053b55b47288c7f562f5460
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-09 19:11:51 +0000

    found request ID using .get

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script to take in a url, send a request to the URL, and
+displays the value of the X-Request-Id variable """
+
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+if sys.argv is not None:
+    with urllib.request.urlopen(url) as response:
+        page = response.info()
+        print(page.get("X-Request-ID"))
