cf98ba3fe9466b2c8cc584c4d75290ed7f2367e0
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-09 18:12:16 +0000

    printing data of html

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module is to request data about a URL """
+
+
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print ("\t- type: {}".format(type(html)))
+    print ("\t- content: {}".format(str(html)))
+    print ("\t- utf8 content: {}".format(html, encoding='utf-8'))
