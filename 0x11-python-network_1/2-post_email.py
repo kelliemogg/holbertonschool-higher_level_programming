651c51a4ef197370808993de56f452e6adc5e675
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-09 20:48:11 +0000

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script to take in a url, send a request to the URL, and
+displays the value of the X-Request-Id variable """
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    data = {}
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    data = data.encode('UTF-8')
+    req = urllib.request.Request(url, email)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print(page.decode('UTF-8'))
