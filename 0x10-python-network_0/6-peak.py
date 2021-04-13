becec9b9731303a4be5b2d6e97c22bbfed7e9087
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-13 19:43:39 +0000

    base

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,23 +3,5 @@
 
 
 def find_peak(list_of_integers):
-
-    int left, right:
-    mid = (left + right) // 2
-
-    # check if the middle element is greater than its neighbors
-    if ((mid == 0 or A[mid - 1] <= A[mid]) and
-            (mid == len(A) - 1 or A[mid + 1] <= A[mid])):
-        return mid
-
-    # If the left neighbor of `mid` is greater than the middle element,
-    # find the peak recursively in the left sublist
-    if mid - 1 >= 0 and A[mid - 1] > A[mid]:
-        return findPeakElement(A, left, mid - 1)
-
-    # If the right neighbor of `mid` is greater than the middle element,
-    # find the peak recursively in the right sublist
-    return findPeakElement(A, mid + 1, right)
-
-
-if __name__ == '__main__':
+    """ find peak of an array of ints """
+    return(max(list_of_integers))
