a8b1af3d5475ed0689500e443db0fa15cd8dd7d2
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-13 16:17:25 +0000

    removed h flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL and sends a POST request to the URL
-curl -s -X POST -H -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
